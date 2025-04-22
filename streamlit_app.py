de8a8de21fccee099cd834dbf3bdb57885e4d478
andreasbrondrup-bearingpoint <andreas.brondrup@bearingpoint.com>
2025-04-22 10:46:14 +0200

    Update streamlit_app.py
    return response as df

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,4 +49,5 @@ if ingredient_list and name_on_order:
         st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
+#st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
