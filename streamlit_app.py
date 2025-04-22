21e6915f7df994ac18d86eb42b0d6cbd3c39e353
andreasbrondrup-bearingpoint <andreas.brondrup@bearingpoint.com>
2025-04-22 10:48:25 +0200

    Update streamlit_app.py
    move api call to if statement

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,6 +31,8 @@ if ingredient_list and name_on_order:
 
     for x in ingredient_list:
         ingredients_string += x + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     #st.write(ingredients_string)
 
@@ -48,6 +50,3 @@ if ingredient_list and name_on_order:
 
         st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
